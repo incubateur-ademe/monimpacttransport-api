351a371703a2c34c0db10270f237a88a5ecfd23a
Florian <hi@florianpanchout.com>
2021-06-09 18:28:31 +0200

    Fix

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
-const port = 5000
+const db = require('./queries')
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.listen(process.env.PORT || 5000, () => {
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+
+app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
