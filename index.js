c5cb9920812065498c47faf117ef88ce30ff4d37
Florian <hi@florianpanchout.com>
2021-06-09 18:25:12 +0200

    Change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
