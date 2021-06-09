31b8adfec31d7b275b9e51777bb96897418c3367
Florian <hi@florianpanchout.com>
2021-06-09 17:59:39 +0200

    Test

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-
 const app = express()
-//const db = require('./queries')
 const port = 3000
-
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
-/*
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-*/
+
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
