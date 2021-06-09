d5b757fbb147ea5afcd5aa456c8ac5415375bb2b
Florian <hi@florianpanchout.com>
2021-06-09 18:26:25 +0200

    Use Env Port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`App running on port ${port}.`)
 })
